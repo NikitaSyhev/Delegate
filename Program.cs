7e899fffada724d2f3ec1a5f990707a133b72fd0
Academy_ekat@itstep.academy <Academy_ekat@itstep.academy>
2023-04-28 20:26:12 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Delegate
+{
+
+    // объявили делегат
+    delegate void output(params object[] values); //params - модификатор, который говорит что количество значений ////может  быть неопределено
+    internal static class Program
+    {
+
+        static void NamesOfObj(object[] _arr)   //метод
+        {
+            string output = string.Empty; // строка нулевая 
+            foreach (var item in _arr)
+            {
+                output += $"{item.GetType()}\n";
+            }
+            MessageBox.Show(output);
+        }
+        static void Main()
+        {
+
+            output myPrint = (object[] _arr) =>// переменная делегата
+            {
+                foreach (var item in _arr)
+                {
+                    Console.WriteLine(item);
+                }
+            };
+            myPrint += NamesOfObj;    // передаем адрес метода ( NamesOfObj )
+            myPrint += (object[] _arr) =>   // метод без названия
+            {
+                using (var sw = new StreamWriter("output.txt", true)) // запись в файл
+                {   
+                    foreach (var item in _arr)
+                    {
+                        sw.WriteLine(item.ToString(), true);  
+                    }
+                }
+            };
+            myPrint(1, "ldf", 3.5f, 'F'); // вызываем метод
+            myPrint -= NamesOfObj;
+            myPrint(1, 2d, (byte)25, true);
+
+
+              Console.ReadLine();
+        }
+            
+          
+        }
+    }
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Delegate")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Delegate")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("8309e152-2569-4a7e-9435-a2e1a7bb7f1b")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
